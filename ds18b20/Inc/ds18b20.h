96e947242fe99baeea8deaeaaa7d9970d0d186f9
stDstm <istarikstd@gmail.com>
2019-05-05 23:17:12 +0300

    ds18b20

---
+++ ds18b20/Inc/ds18b20.h
@@ -0,0 +1,49 @@
+/*
+ * ds18b20.h
+ *
+ *  Created on: 5 мая 2019 г.
+ *      Author: dima
+ */
+
+#ifndef DS18B20_H_
+#define DS18B20_H_
+
+#include "main.h"
+
+#define DWT_CONTROL *(volatile unsigned long *)0xE0001000
+#define SCB_DEMCR *(volatile unsigned long *)0xE000EDFC
+
+#define DELAY_RESET           500
+#define DELAY_WRITE_0         60
+#define DELAY_WRITE_0_PAUSE   10
+#define DELAY_WRITE_1         10
+#define DELAY_WRITE_1_PAUSE   60
+#define DELAY_READ_SLOT       10
+#define DELAY_BUS_RELAX       10
+#define DELAY_READ_PAUSE      50
+#define DELAY_T_CONVERT       760000
+#define DELAT_PROTECTION      5
+
+typedef enum {
+	SKIP_ROM = 0xCC,
+	CONVERT_T = 0x44,
+	READ_SCRATCHPAD = 0xBE,
+	WRITE_SCRATCHPAD = 0x4E,
+	TH_REGISTER = 0x4B,
+	TL_REGISTER = 0x46,
+} COMMANDS;
+
+typedef enum {
+	DS18B20_Resolution_9_bit 	= 0x1F,
+	DS18B20_Resolution_10_bit 	= 0x3F,
+	DS18B20_Resolution_11_bit 	= 0x5F,
+	DS18B20_Resolution_12_bit 	= 0x7F
+} DS18B20_Resolution;
+
+void DWT_Init(void);
+void delay_us(uint32_t us);
+void Init_ds18b20(DS18B20_Resolution resolution);
+void setResolution(DS18B20_Resolution resolution);
+uint16_t ds18b20_getTemperature();
+
+#endif /* DS18B20_H_ */
---
+++ ds18b20/Src/ds18b20.c
@@ -0,0 +1,127 @@
+/*
+ * ds18b20.c
+ *
+ *  Created on: 5 мая 2019 г.
+ *      Author: dima
+ */
+
+
+#include "ds18b20.h"
+
+uint32_t DELAY_WAIT_CONVERT = DELAY_T_CONVERT;
+
+void DWT_Init(void)
+{
+    SCB_DEMCR |= CoreDebug_DEMCR_TRCENA_Msk; // разрешаем использовать счётчик
+	DWT_CONTROL |= DWT_CTRL_CYCCNTENA_Msk;   // запускаем счётчик
+}
+
+void delay_us(uint32_t us)
+{
+    uint32_t us_count_tic =  us * (SystemCoreClock / 1000000);
+    DWT->CYCCNT = 0U; // обнуляем счётчик
+    while(DWT->CYCCNT < us_count_tic);
+}
+
+void Init_ds18b20(DS18B20_Resolution resolution)
+{
+	DWT_Init();
+	setResolution(resolution);
+}
+
+void reset()
+{
+	HAL_GPIO_WritePin(DSpin_GPIO_Port, DSpin_Pin, GPIO_PIN_RESET);
+	delay_us(DELAY_RESET);
+	HAL_GPIO_WritePin(DSpin_GPIO_Port, DSpin_Pin, GPIO_PIN_SET);
+	delay_us(DELAY_RESET);
+}
+
+uint8_t getDevider(DS18B20_Resolution resolution)
+{
+	uint8_t devider;
+	switch (resolution)
+	{
+		case DS18B20_Resolution_9_bit:
+			devider = 8;
+			break;
+		case DS18B20_Resolution_10_bit:
+			devider = 4;
+			break;
+		case DS18B20_Resolution_11_bit:
+			devider = 2;
+			break;
+		case DS18B20_Resolution_12_bit:
+		default:
+			devider = 1;
+			break;
+	}
+
+	return devider;
+}
+
+void writeBit(uint8_t bit)
+{
+	HAL_GPIO_WritePin(DSpin_GPIO_Port, DSpin_Pin, GPIO_PIN_RESET);
+	delay_us(bit ? DELAY_WRITE_1 : DELAY_WRITE_0);
+	HAL_GPIO_WritePin(DSpin_GPIO_Port, DSpin_Pin, GPIO_PIN_SET);
+	delay_us(bit ? DELAY_WRITE_1_PAUSE : DELAY_WRITE_0_PAUSE);
+}
+
+void writeByte(uint8_t data)
+{
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		writeBit(data >> i & 1);
+		delay_us(DELAT_PROTECTION);
+	}
+}
+
+uint8_t readBit()
+{
+	uint8_t bit = 0;
+	HAL_GPIO_WritePin(DSpin_GPIO_Port, DSpin_Pin, GPIO_PIN_RESET);
+	delay_us(DELAY_READ_SLOT);
+	HAL_GPIO_WritePin(DSpin_GPIO_Port, DSpin_Pin, GPIO_PIN_SET);
+	delay_us(DELAY_BUS_RELAX);
+	bit = (HAL_GPIO_ReadPin(DSpin_GPIO_Port, DSpin_Pin) ? 1 : 0);
+	delay_us(DELAY_READ_PAUSE);
+	return bit;
+}
+
+uint16_t readTemperature()
+{
+	uint16_t data = 0;
+	for (uint8_t i = 0; i < 16; i++)
+	{
+		data += (uint16_t) readBit() << i;
+	}
+
+	//return  (uint16_t)(((float) data / 16.0) * 10.0);
+	return data;
+}
+
+uint16_t ds18b20_getTemperature()
+{
+	reset();
+	writeByte(SKIP_ROM);
+	writeByte(CONVERT_T);
+	delay_us(DELAY_WAIT_CONVERT);
+
+	reset();
+	writeByte(SKIP_ROM);
+	writeByte(READ_SCRATCHPAD);
+
+	return readTemperature();
+}
+
+void setResolution(DS18B20_Resolution resolution)
+{
+	reset();
+	writeByte(SKIP_ROM);
+	writeByte(WRITE_SCRATCHPAD);
+	writeByte(TH_REGISTER);
+	writeByte(TL_REGISTER);
+	writeByte(resolution);
+	DELAY_WAIT_CONVERT = DELAY_T_CONVERT / getDevider(resolution);
+}
