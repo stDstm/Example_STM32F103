b2493b585bcee0d65c6049ad992b771b0af3a8a0
stDstm <istarikstd@gmail.com>
2021-10-15 19:23:11 +0300

    New

---
+++ CAN_Filter/README.md
@@ -0,0 +1,14 @@
+Для стандартных идентификаторов:
+
+gcc -Wextra -Wall bits_mask_11.c -o bits_mask_11
+
+./bits_mask_11 0x0480 0x07F8
+
+Для расширенных:
+
+gcc -Wextra -Wall bits_mask_29.c -o bits_mask_29
+
+./bits_mask_29 0x00010000 0x1FFFFFF8
+
+
+[![Donate](https://istarik.ru/uploads/images/00/00/01/2020/04/12/ff1b11.png)](https://istarik.ru/don.html)
---
+++ CAN_Filter/bits_mask_29.c
@@ -0,0 +1,69 @@
+
+//#include <windows.h>
+#include <string.h>
+#include <stdio.h>  
+#include <stdlib.h>
+#include <stdint.h>
+
+#define SIZE_BUF_BITS	33
+
+
+void print_bits(char *buf_bits, uint32_t dig)
+{
+	memset(buf_bits, 0, SIZE_BUF_BITS);
+	
+	uint8_t num_bits = 32;
+	uint8_t b = 3;
+
+	for(uint8_t i = 0; i < (num_bits - 3); i++)
+	{
+		buf_bits[i] = (dig & (1 << (num_bits - 1 - b))) > 0 ? '1' : '0';
+		b++;
+	}
+}
+
+
+int main(int argc, char *argv[])  
+{  
+	if(argc != 3) 
+	{
+		printf("Error arg\n");
+		exit(0);
+	}
+	
+	uint32_t ID = strtoul(argv[1], NULL, 0);
+	uint32_t Mask = strtoul(argv[2], NULL, 0);
+	
+	char buf_bits[SIZE_BUF_BITS] = {0,}; // буфер в который запишутся биты
+
+	print_bits(buf_bits, ID);
+
+	printf("\n   HEX                  BIN\n");
+	printf("-----------------------------------------\n");
+	printf("0x%08X  %s    ID - первый регистр фильтра\n", ID, buf_bits);
+	
+	print_bits(buf_bits, Mask);
+
+	printf("0x%08X  %s    Mask - второй регистр фильтра\n\n", Mask, buf_bits);
+
+	printf("ID которые будут приняты\n");
+	printf("-------------------------\n");
+ 
+ 
+	for(uint32_t i = 0; i < 0x200000; i++)
+	{
+		if((i & Mask) == ID) 
+		{
+			print_bits(buf_bits, i);
+			printf("0x%08X  %s\n", i, buf_bits);
+		}
+	}
+	
+	return 0;
+}
+
+
+// gcc -Wextra -Wall bits_mask_29.c -o bits_mask_29
+
+// ./bits_mask_29 0x00010000 0x1FFFFFFF
+
